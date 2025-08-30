bea9ba26f5867b4af71ed2f1a9f599b985857608
user.email "dhruvknipsss023@gmail.com <dhruvknipsss023@gmail.com>
2025-08-31 01:22:57 +0530

    adding a updating status feature from the recruiter side

--- src/components/application-card.jsx
+++ src/components/application-card.jsx
@@ -9,6 +9,13 @@ import {
 import useFetch from "@/hooks/use-fetch";
 import { updateApplicationStatus } from "@/api/apiApplications";
 import { BarLoader } from "react-spinners";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 
 const ApplicationCard = ({ application, isCandidate = false }) => {
   const handleDownload = () => {
@@ -18,20 +25,24 @@ const ApplicationCard = ({ application, isCandidate = false }) => {
     link.click();
   };
 
-  const { loading: loadingHiringstatus, fn: fnHiringStatus } = useFetch(
+  const { loading: loadingHiringStatus, fn: fnHiringStatus } = useFetch(
     updateApplicationStatus,
     {
       job_id: application.job_id,
     }
   );
 
+  const handleStatusChange = (status) => {
+    fnHiringStatus(status).then(() => fnHiringStatus());
+  };
+
   return (
     <Card>
-      {!loadingHiringstatus && <BarLoader width={"100%"} color="#36d7b7" />}
+      {loadingHiringStatus && <BarLoader width={"100%"} color="#36d7b7" />}
       <CardHeader>
         <CardTitle className="flex justify-between font-bold">
           {isCandidate
-            ? `${application?.job.title} at ${application?.job?.company?.name}`
+            ? `${application?.job?.title} at ${application?.job?.company?.name}`
             : application?.name}
           <Download
             size={18}
@@ -40,34 +51,45 @@ const ApplicationCard = ({ application, isCandidate = false }) => {
           />
         </CardTitle>
       </CardHeader>
-
       <CardContent className="flex flex-col gap-4 flex-1">
         <div className="flex flex-col md:flex-row justify-between">
           <div className="flex gap-2 items-center">
-            <BriefcaseBusiness size={15} />
-            {application?.experience} years of experience
+            <BriefcaseBusiness size={15} /> {application?.experience} years of
+            experience
           </div>
           <div className="flex gap-2 items-center">
             <School size={15} />
             {application?.education}
           </div>
           <div className="flex gap-2 items-center">
-            <Boxes size={15} />
-            Skills : {application?.skills}
+            <Boxes size={15} /> Skills: {application?.skills}
           </div>
         </div>
         <hr />
-        <CardFooter className="flex justify-between">
-          <span> {new Date(application?.created_at).toLocaleString()} </span>
-          {!isCandidate ? (
-            <span className="capitalize font-bold">
-              Status: {application?.status}
-            </span>
-          ) : (
-            <></>
-          )}
-        </CardFooter>
       </CardContent>
+      <CardFooter className="flex justify-between">
+        <span>{new Date(application?.created_at).toLocaleString()}</span>
+        {isCandidate ? (
+          <span className="capitalize font-bold">
+            Status: {application.status}
+          </span>
+        ) : (
+          <Select
+            onValueChange={handleStatusChange}
+            defaultValue={application.status}
+          >
+            <SelectTrigger className="w-52">
+              <SelectValue placeholder="Application Status" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="applied">Applied</SelectItem>
+              <SelectItem value="interviewing">Interviewing</SelectItem>
+              <SelectItem value="hired">Hired</SelectItem>
+              <SelectItem value="rejected">Rejected</SelectItem>
+            </SelectContent>
+          </Select>
+        )}
+      </CardFooter>
     </Card>
   );
 };
