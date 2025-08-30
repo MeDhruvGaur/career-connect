2511bac10c86479bb5c42bc3df42241e35b8fef2
user.email "dhruvknipsss023@gmail.com <dhruvknipsss023@gmail.com>
2025-08-30 22:09:45 +0530

    Add updation code in apiApplications.js

--- src/api/apiApplications.js
+++ src/api/apiApplications.js
@@ -32,3 +32,19 @@ export async function applyToJob(token, _, jobData) {
 
   return data;
 }
+
+//Updation
+export async function updateApplications(token, { job_id }, status) {
+  const supabase = await supabaseClient(token);
+  let { data, error } = await supabase
+    .from("applications")
+    .update({ status })
+    .eq("job_id", job_id)
+    .select();
+
+  if (error || data.length === 0) {
+    console.error("Error Updating Application Status:", error);
+    return null;
+  }
+  return data;
+}
